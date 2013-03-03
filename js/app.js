9498ba2b2b78a35781b293e6fe549d6761a66aef
bretto <separ8@gmail.com>
2013-03-03 20:53:00 +1000

    added flickr

--- js/app.js
+++ js/app.js
@@ -51,23 +51,64 @@ angular.module('App', ['App.filters', 'App.services', 'App.directives', 'App.con
     });
 
 
+//var productsPromise = function ($q, $route, $http, $log, $stateParams, $timeout) {
+//
+//    function makeObj(i){
+//        return {title:'Product: ' + i}
+//    }
+//
+//    var deferred = $q.defer();
+//    var promiseData = [];
+//
+//    for (var i = 0; i < 10; i++) {
+//        promiseData.push(makeObj(i));
+//    }
+//
+//    $timeout(function(){deferred.resolve(promiseData); },0);
+//
+//    return deferred.promise;
+//}
+
+
 var productsPromise = function ($q, $route, $http, $log, $stateParams, $timeout) {
 
-    function makeObj(i){
-        return {title:'Product: ' + i}
-    }
+    var api_key, deferred, params, url;
+    deferred = $q.defer();
+    api_key = '2bb0b524a3e3cbb9ceaea74b30dabf93';
+    url = 'http://api.flickr.com/services/rest/';
+    params = {
+        method: 'flickr.photos.search',
+        api_key: api_key,
+        text: 'cameleon',
+        per_page: 22,
+        format: 'json',
+        page: 1,
+        jsoncallback: 'JSON_CALLBACK'
+    };
+    $http.jsonp(url, {
+        params: params
+    }).success(function(data, status, headers, config) {
+            var page_info, photos;
+            page_info = {};
+            page_info.page = data.photos.page;
+            page_info.pages = data.photos.pages;
 
-    var deferred = $q.defer();
-    var promiseData = [];
-
-    for (var i = 0; i < 10; i++) {
-        promiseData.push(makeObj(i));
-    }
-
-    $timeout(function(){deferred.resolve(promiseData); },0);
-
+            photos = $.map(data.photos.photo, function(photo) {
+                return {
+                    title: photo.title,
+                    thumb_src: "http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_s.jpg",
+                    src: "http://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + ".jpg"
+                };
+            });
+            return deferred.resolve(photos);
+        });
     return deferred.promise;
 }
+
+
+
+
+
 
 var productPromise = function ($q, $route, $http, $log, $stateParams, $timeout) {
 
